12c2780c84deb2c8b742749f15da7d73dde06442
gkurkjian <gkurkjian@myseneca.ca>
2023-03-29 18:43:01 -0400

    img practice component added

---
+++ pages/component/ShowImg.js
@@ -0,0 +1,13 @@
+export default function ShowImg() {
+    let imgURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbVHu5rXAK8KUSep6zlSxc_hg5m1oMLsvvC1sGRDeuFrMyoZFK52QShTR0F3hza4pMG1w&usqp=CAU";
+    
+    return (
+        <>
+            {/* This way it'll not work because there's no curly brackets wrapped up */}
+            {/* <img src="imgURL" alt="seneca-college-img" /> */}
+
+            {/* This is the right way to pass the values in JSX */}
+            <img src={imgURL} alt={"seneca-college-img"} />
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import SayHello from './component/SayHello'
 import SayMessage from './component/SayMessage'
+import ShowImg from './component/ShowImg'
 
 export default function Home() {
   return (
     <div>
       <SayHello />
-      <SayMessage msg="Welcome WEB422" exclamation="!"/>
+      {/* We can pass by string too */}
+      <SayMessage msg={'Welcome WEB422'} exclamation="!"/>
       <SayMessage msg="Next.js is Awesome" exclamation="!!!"/>
       <SayMessage />
+      <ShowImg />
     </div>
   )
 }

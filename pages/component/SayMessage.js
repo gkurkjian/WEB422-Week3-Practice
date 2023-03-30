da5abfb2c2612e2d39554d8ec9d83260be825087
gkurkjian <gkurkjian@myseneca.ca>
2023-03-30 14:09:56 -0400

    included ShowImg component in SayMessage to test

--- pages/component/SayMessage.js
+++ pages/component/SayMessage.js
@@ -1,8 +1,11 @@
+import ShowImg from "./ShowImg"
+
 // intro to props, here it's taking 2 props
 export default function SayMessage(props) {
     return (
         <>
             <p>{props.msg}{props.exclamation}</p>
+            <ShowImg />
         </>
     )
 }
